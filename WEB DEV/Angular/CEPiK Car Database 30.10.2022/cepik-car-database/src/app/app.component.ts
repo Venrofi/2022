e8740c237b4f2e849504b3026dda59e73b24687b
Venrofi <radoslaw.terelak@gmail.com>
2022-11-01 08:58:42 +0100

    Initial work on API

--- WEB DEV/Angular/CEPiK Car Database 30.10.2022/cepik-car-database/src/app/app.component.ts
+++ WEB DEV/Angular/CEPiK Car Database 30.10.2022/cepik-car-database/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{ Constants } from './config/constants';
+import { ApiHttpService } from './services/api-http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ApiHttpService]
 })
-export class AppComponent {
-  title = 'cepik-car-database';
+export class AppComponent implements OnInit{
+    title = Constants.TitleOfSite;
+    data: any;
+
+    constructor(api: ApiHttpService) {
+      // this.data = api.get('https://jsonplaceholder.typicode.com/posts');
+      this.data = api.get('https://api.cepik.gov.pl/pojazdy');
+      console.log(this.data);
+    }
+    ngOnInit() {
+        console.log(this.title);
+    }
 }
