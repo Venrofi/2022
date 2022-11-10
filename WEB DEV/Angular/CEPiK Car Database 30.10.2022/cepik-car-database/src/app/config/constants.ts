7fc0d221c8fa6ee1f0861ba8c9e8f2df831ac8eb
Venrofi <radoslaw.terelak@gmail.com>
2022-11-10 12:58:50 +0100

    Fix for API endpoint

--- WEB DEV/Angular/CEPiK Car Database 30.10.2022/cepik-car-database/src/app/config/constants.ts
+++ WEB DEV/Angular/CEPiK Car Database 30.10.2022/cepik-car-database/src/app/config/constants.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-  public static API_ENDPOINT: string = 'https://api.cepik.gov.pl/';
+  public static API_ENDPOINT: string = 'https://api.cepik.gov.pl';
   public static API_PROXY_ENDPOINT: string = 'http://localhost:3000';
   public static TitleOfSite: string = 'CEPiK Car Database';
 }
